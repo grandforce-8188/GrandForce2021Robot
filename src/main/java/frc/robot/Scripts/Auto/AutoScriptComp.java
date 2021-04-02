4643b72ae0eacedc2934d226b82be4082ef6dea8
Grand Force <grandforce8188@gmail.com>
2021-04-02 18:05:17 -0500

    Added autonomous engine support

---
+++ src/main/java/frc/robot/Scripts/Auto/AutoScriptComp.java
@@ -0,0 +1,17 @@
+package frc.robot.Scripts.Auto;
+
+public class AutoScriptComp
+{
+    public static String script = "{\n" +
+            " \"instructions\":[\n" +
+            "{\n" +
+            "  \"instruction\": \"reverse\",\n" +
+            "  \"parameter\": 2.0\n" +
+            "},\n" +
+            "{\n" +
+            "  \"instruction\": \"shoot\",\n" +
+            "  \"parameter\": 0.0\n" +
+            "}\n" +
+            "]\n" +
+            "}";
+}
